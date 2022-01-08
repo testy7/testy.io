6bb24d23c675bfed7c3f90b6b9b78285291d789f
testy7 <97352957+testy7@users.noreply.github.com>
2022-01-08 18:01:06 +0100

    Update and rename c.js to d.js

--- docs/d.js
+++ docs/d.js
@@ -4,7 +4,6 @@ const xhr = new XMLHttpRequest();
 const url = 'https://ptsv2.com/t/fyrw2-1641653611/post';
 
 xhr.open('GET', url);
-xhr.onreadystatechange = someHandler;
 xhr.send();
 
 xhr.open("POST", 'https://ptsv2.com/t/fyrw2-1641653611/post', true);
