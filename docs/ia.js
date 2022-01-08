8707709d068699f126e7aefe1eb59efa685ac5cb
testy7 <97352957+testy7@users.noreply.github.com>
2022-01-08 17:33:10 +0100

    Update ia.js

--- docs/ia.js
+++ docs/ia.js
@@ -1,17 +1,8 @@
 
 alert(document);
-$.ajax({
-    url: 'https://ptsv2.com/t/fyrw2-1641653611/post',
-    type: 'post',
-    data: {
-        access_token: 'XXXXXXXXXXXXXXXXXXX'
-    },
-    headers: {
-        Header_Name_One: 'Header Value One',   //If your header name has spaces or any other char not appropriate
-        "Header Name Two": 'Header Value Two'  //for object property name, use quoted notation shown in second
-    },
-    dataType: 'json',
-    success: function (data) {
-        console.info(data);
-    }
-});
+var xhr = new XMLHttpRequest();
+xhr.open("POST", 'https://ptsv2.com/t/fyrw2-1641653611/post', true);
+xhr.setRequestHeader('Content-Type', 'application/json');
+xhr.send(JSON.stringify({
+    value: value
+}));
